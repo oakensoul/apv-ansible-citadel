d79d8f9a734618b67cfd19b5667c39866eaad9ee
Robert G. Johnson Jr <github@oakensoul.com>
2016-05-27 19:13:11 -0400

    Adding scripts from apv tutorial

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,11 +1,24 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# Delete unneeded files.
-rm -f /home/vagrant/*.sh
+# Removing leftover leases and persistent rules
+echo "cleaning up dhcp leases"
+rm /var/lib/dhcp/*
 
-# Zero out the rest of the free space using dd, then delete the written file.
+# Make sure Udev doesn't block our network
+echo "cleaning up udev rules"
+rm /etc/udev/rules.d/70-persistent-net.rules
+mkdir /etc/udev/rules.d/70-persistent-net.rules
+rm -rf /dev/.udev/
+rm /lib/udev/rules.d/75-persistent-net-generator.rules
+
+echo "Adding a 2 sec delay to the interface up, to make the dhclient happy"
+echo "pre-up sleep 2" >> /etc/network/interfaces
+
+# Zero out the free space to save space in the final image:
 dd if=/dev/zero of=/EMPTY bs=1M
 rm -f /EMPTY
 
-# Add `sync` so Packer doesn't quit too early, before the large file is deleted.
+# Sync to ensure that the delete completes before this moves on.
+sync
+sync
 sync
